38bda84641149644ff38b9a092eecf9f28dfc74c
qnaiv <nochia1992@gmail.com>
2019-06-20 22:10:15 +0900

    update Articles: 日付表示追加

--- src/.vuepress/components/Articles.vue
+++ src/.vuepress/components/Articles.vue
@@ -1,17 +1,21 @@
 <template>
   <div>
     <div v-for="page in filteredPages" class="article-container" v-if="!page.frontmatter.exclude">
-      <h3 class="article-title">
-        <router-link :to="page.path">{{page.title || 'No Title'}}</router-link>
-      </h3>
-      <div class="tag-container">
-        <i class="fas fa-tags tag-icon"></i>
-        <div v-for="c in page.frontmatter.tags" class="tag">{{c}}</div>
-      </div>
-      <div
-        v-if="page.frontmatter.description"
-        class="article-description"
-      >{{page.frontmatter.description}}</div>
+        <div class="article-date">
+<!--           {{page.createDate || '&nbsp;'}}
+ -->          
+          <div class="create-month">JUN</div>
+          <div class="create-date">20</div>
+        </div>
+        <div class="article-body">
+          <h3 class="article-title">
+            <router-link :to="page.path">{{page.title || 'No Title'}}</router-link>
+          </h3>
+          <div class="tag-container">
+            <i class="fas fa-tags tag-icon"></i>
+            <div v-for="c in page.frontmatter.tags" class="tag">{{c}}</div>
+          </div>
+        </div>
     </div>
   </div>
 </template>
@@ -31,8 +35,28 @@ export default {
 </script>
 
 <style>
+.article-container{
+display: grid;
+grid-template-columns: 50px auto;
+grid-template-areas: "article-date article-body";
+margin-bottom: 6px;
+}
+.article-date{
+grid-area: article-date;
+}
+
+.article-date .create-month{
+  font-size: 0.8em;
+}
+.article-date .create-date{
+  font-size: 1.4em;
+}
+.article-body{
+grid-area: article-body;
+}
+
 h3 {
-  margin: 10px 0 0 0;
+  margin: 0;
 }
 .tag-container {
   min-height: 33px;
