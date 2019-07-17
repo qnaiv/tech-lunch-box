1dc3d89d81c9ff2fcc26a43101058a5171bbe5e0
qnaiv <qqnaiv@gmail.com>
2019-07-17 16:38:51 +0900

    'deploy'

--- deploy.bat
+++ deploy.bat
@@ -1,15 +1,15 @@
 @echo off
 cd /d %~dp0
 
-npm run build
+call npm run build
 
 cd docs
 
 echo lunchbox.qnv.pw > CNAME
 
-git add -A
-git commit -m 'deploy'
+call git add -A
+call git commit -m 'deploy'
 
-git push origin master
+call git push origin master
 
 cd ..
