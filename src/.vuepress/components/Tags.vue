74f2cae6ad17b4c7f4148288e861d1f0f09a98c4
qnaiv <qqnaiv@gmail.com>
2019-07-17 15:31:47 +0900

    comment out js temporarily

--- src/.vuepress/components/Tags.vue
+++ src/.vuepress/components/Tags.vue
@@ -5,36 +5,36 @@
 </template>
 
 <script>
-import moment from "moment";
-import isNil from "lodash-es/isNil";
-import uniq from "lodash-es/uniq";
+// import moment from "moment";
+// import isNil from "lodash-es/isNil";
+// import uniq from "lodash-es/uniq";
 
-export default {
-  name: "Tags",
-  props: ["pages"],
-  computed: {
-    getTags() {
-      return this.pages
-        .filter(page => page.path.includes(this.prefix || ""))
-        .filter(page => !page.path.indexOf("/posts/"))
-        .flatMap(page => {
-          return page.frontmatter.tags;
-        })
-        .filter(tag => !isNil(tag))
-        .filter((elem, index, self) => self.indexOf(elem) === index);
-    }
-  },
-  filters: {
-    createDate: date => {
-      if (isNil(date)) return "";
-      return moment(date).format("DD");
-    },
-    createMonth: date => {
-      if (isNil(date)) return "";
-      return moment(date).format("MMM");
-    }
-  }
-};
+// export default {
+//   name: "Tags",
+//   props: ["pages"],
+//   computed: {
+//     getTags() {
+//       return this.pages
+//         .filter(page => page.path.includes(this.prefix || ""))
+//         .filter(page => !page.path.indexOf("/posts/"))
+//         .flatMap(page => {
+//           return page.frontmatter.tags;
+//         })
+//         .filter(tag => !isNil(tag))
+//         .filter((elem, index, self) => self.indexOf(elem) === index);
+//     }
+//   },
+//   filters: {
+//     createDate: date => {
+//       if (isNil(date)) return "";
+//       return moment(date).format("DD");
+//     },
+//     createMonth: date => {
+//       if (isNil(date)) return "";
+//       return moment(date).format("MMM");
+//     }
+//   }
+// };
 </script>
 
 <style>
